af81acb2bb1a15348857d3024489d783884cca2b
Mat Fukano <fukano.mat@gmail.com>
2022-03-11 06:32:31 -0800

    Bowing down to standard-fix

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,9 @@ const mongo = require('mongojs')
 const mongoose = require('mongoose')
 
 const helmet = require('helmet')
-const requestCountry = require('request-country')
 const config = require('../config')
 const validMongoId = require('../lib/valid-mongoid')
 const { join } = require('path')
-const i18nextMiddleware = require('i18next-express-middleware')
-const Backend = require('i18next-node-fs-backend')
-const i18n = require('../i18n')
 const app = next({ dev })
 const handler = routes.getRequestHandler(app)
 const port = parseInt(process.env.PORT, 10) || 3000
@@ -28,118 +24,91 @@ const userRoutes = require('./routes/users')
 const invitationRoutes = require('./routes/invitations')
 const stripeRoutes = require('./routes/stripe')
 
-i18n
-  .use(Backend)
-  .use(i18nextMiddleware.LanguageDetector)
-  .init(
-    {
-      fallbackLng: 'en',
-      preload: ['en', 'es'], // Preload all languages
-      ns: ['common', 'home'], // Need to preload all the namespaces
-      backend: {
-        loadPath: join(__dirname, '../locales/{{lng}}/{{ns}}.json'),
-        addPath: join(__dirname, '../locales/{{lng}}/{{ns}}.missing.json')
-      }
-    },
-    () => {
-      // Loaded translations we can bootstrap our routes
-      app.prepare().then(() => {
-        const server = express()
-        const db = mongo(config.DB_CONNECTION)
-        const collection = db.collection(config.DB_COLLECTION)
+app.prepare().then(() => {
+  const server = express()
+  const db = mongo(config.DB_CONNECTION)
+  const collection = db.collection(config.DB_COLLECTION)
 
-        mongoose
-          .connect(config.DB_CONNECTION, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-          })
-          .then(console.log(`MongoDB connected ${config.DB_CONNECTION}`))
-          .catch(err => console.log(err))
+  mongoose
+    .connect(config.DB_CONNECTION, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+    .then(console.log(`MongoDB connected ${config.DB_CONNECTION}`))
+    .catch(err => console.log(err))
 
-        if (dev) {
-          mongoose.set('debug', true)
-        }
+  if (dev) {
+    mongoose.set('debug', true)
+  }
 
-        /*
-        The reason this is here is because I was running into an issue querying on User without using the Mongoose model.
-        I know that this gets handled in the routes bootstrapping for User, but is there a better way to handle this?
-        */
-        const User = mongoose.model('User')
+  /*
+  The reason this is here is because I was running into an issue querying on User without using the Mongoose model.
+  I know that this gets handled in the routes bootstrapping for User, but is there a better way to handle this?
+  */
+  const User = mongoose.model('User')
 
-        server.use(helmet())
-        server.use(express.json())
-        server.use(i18nextMiddleware.handle(i18n))
-        server.use('../locales', express.static(join(__dirname, '../locales'))) // Serve locales for client
-        server.use(requestCountry.middleware({
-          privateIpCountry: 'en'
-        }))
+  server.use(helmet())
+  server.use(express.json())
 
-        server.get('../sitemap.xml', (req, res) => {
-          const filePath = join(__dirname, 'static', 'sitemap.xml')
-          return app.serveStatic(req, res, filePath)
-        })
+  /* Alheimsins config stuff */
+  server.get('../sitemap.xml', (req, res) => {
+    const filePath = join(__dirname, 'static', 'sitemap.xml')
+    return app.serveStatic(req, res, filePath)
+  })
 
-        server.get('../service-worker.js', (req, res) => {
-          const filePath = join(__dirname, '.next', 'service-worker.js')
-          return app.serveStatic(req, res, filePath)
-        })
+  server.get('../service-worker.js', (req, res) => {
+    const filePath = join(__dirname, '.next', 'service-worker.js')
+    return app.serveStatic(req, res, filePath)
+  })
 
-        server.post('../locales/add/:lng/:ns', i18nextMiddleware.missingKeyHandler(i18n))
+  // TODO: Results APIs should be compartmentalized into their own routes
+  server.get('/api/get/:id', (req, res) => {
+    const id = req.params && req.params.id ? req.params.id : false
+    if (!id || !validMongoId(id)) throw new Error('Not a valid id')
+    collection.findOne({ _id: mongo.ObjectId(id) }, (error, data) => {
+      if (error) throw error
+      res.send(data)
+    })
+  })
 
-        server.get('/api/get/:id', (req, res) => {
-          const id = req.params && req.params.id ? req.params.id : false
-          if (!id || !validMongoId(id)) throw new Error('Not a valid id')
-          collection.findOne({ _id: mongo.ObjectId(id) }, (error, data) => {
-            if (error) throw error
-            res.send(data)
+  server.post('/api/save', (req, res) => {
+    const token = req.body.user && req.body.user.token ? req.body.user.token.split('.') : false
+    if (!token) throw new Error('Not a valid query')
+    const userId = JSON.parse(Buffer.from(token[1], 'base64').toString('ascii')).id
+
+    if (userId) {
+      User.findOne({ _id: mongo.ObjectId(userId) })
+        .exec()
+        .then((user) => {
+          user.results.push(req.body.result)
+          user.save((err) => {
+            if (err) throw err
           })
         })
-
-        /*
-          Takes in the updated body, locates user based on EMAIL (yikes, could we handle this with token instead?),
-          then continues to save the result in the results collection as normal. There has to be a way to insert
-          this into the User collection without explicitly requiring the mongoose model after mongoose setup, right?
-        */
-        server.post('/api/save', (req, res) => {
-          const token = req.body.user && req.body.user.token ? req.body.user.token.split('.') : false
-          if (!token) throw new Error('Not a valid query')
-          const userId = JSON.parse(Buffer.from(token[1], 'base64').toString('ascii')).id
-
-          if (userId) {
-            User.findOne({ _id: mongo.ObjectId(userId) })
-              .exec()
-              .then((user) => {
-                user.results.push(req.body.result)
-                user.save((err) => {
-                  if (err) throw err
-                })
-              })
-              .catch(err => { throw new Error(err) })
-          }
-
-          const payload = req.body.result
-          collection.insert(payload, (error, data) => {
-            if (error) throw error
-            res.send(data)
-          })
-        })
-
-        // api/users routes mounted
-        server.use('/api/', userRoutes)
-
-        // api/invitations routes mounted
-        server.use('/api/', invitationRoutes)
-
-        // api/stripe routes mounted
-        server.use('/api/', stripeRoutes)
-
-        server.use(handler)
-
-        server.listen(port, (err) => {
-          if (err) throw err
-          console.log(`> Ready on http://localhost:${port}`)
-        })
-      })
+        .catch(err => { throw new Error(err) })
     }
-  )
+
+    const payload = req.body.result
+    collection.insert(payload, (error, data) => {
+      if (error) throw error
+      res.send(data)
+    })
+  })
+
+  // api/users routes mounted
+  server.use('/api/', userRoutes)
+
+  // api/invitations routes mounted
+  server.use('/api/', invitationRoutes)
+
+  // api/stripe routes mounted
+  server.use('/api/', stripeRoutes)
+
+  server.use(handler)
+
+  server.listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port} - env ${process.env.NODE_ENV}`)
+  })
+})
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,5 @@
 import { Router } from '../routes'
-import { Select, Button, Field } from '../components/alheimsins'
-
-import ISO6391 from 'iso-639-1'
-import { getInfo } from '@alheimsins/b5-johnson-120-ipip-neo-pi-r'
-import withI18next from '../hoc/withI18next'
-
-const { languages } = getInfo()
-const langCodes = languages.map(lang => lang.id)
-const languageList = langCodes.map(code => ({ code, name: ISO6391.getName(code) }))
+import { Button } from '../components/alheimsins'
 
 const startTest = e => {
   e.preventDefault()
@@ -15,8 +7,7 @@ const startTest = e => {
   Router.pushRoute(`/test/${lang}`)
 }
 
-const Home = ({ countryCode, t }) => {
-  const defaultLanguage = langCodes.includes(countryCode) ? countryCode : 'en'
+const Home = () => {
   return (
     <div>
       <div style={{ paddingBottom: '20px', borderBottom: '1px solid rgb(234, 234, 234)' }}>
@@ -25,8 +16,8 @@ const Home = ({ countryCode, t }) => {
       <div style={{ textAlign: 'left', margin: 'auto', fontSize: '14px', width: '100%' }}>
         <form onSubmit={startTest}>
           <p>PLEASE READ ALL OF THE INSTRUCTIONS BEFORE TAKING THE TEST. This will ensure you get the greatest value from it possible.</p>
-          <p>The goal of this test isn’t to make you feel good, or only highlight your best qualities so you leave feeling positive about your unique way of shining in this world. If you reply honestly and that is the result, that is wonderful. However, the goal of this test is to allow you to see yourself as you truly are so that you have solid footing upon which to leverage your existing strengths and knowledge of your vulnerabilities so you can improve upon them if desired. </p>
-          <p>We know at this point you are likely excited to take the test and get your results as soon as possible, but it is important to first make sure you are in a state that will allow you to answer the questions with the least bias possible. Read the following instructions and if you are not able to meet these criteria at the moment, we urge you to wait until you can or schedule a time where you know that you’ll be in a more ideal state for providing honest and uninfluenced answers.</p>
+          <p>The goal of this test isn't to make you feel good, or only highlight your best qualities so you leave feeling positive about your unique way of shining in this world. If you reply honestly and that is the result, that is wonderful. However, the goal of this test is to allow you to see yourself as you truly are so that you have solid footing upon which to leverage your existing strengths and knowledge of your vulnerabilities so you can improve upon them if desired. </p>
+          <p>We know at this point you are likely excited to take the test and get your results as soon as possible, but it is important to first make sure you are in a state that will allow you to answer the questions with the least bias possible. Read the following instructions and if you are not able to meet these criteria at the moment, we urge you to wait until you can or schedule a time where you know that you'll be in a more ideal state for providing honest and uninfluenced answers.</p>
           <ol>
             <li>Make sure you are NOT in a heightened level of stress, and that you can think/reflect clearly. Have a snack, a glass of water, and take five deep breaths to clear your mind before diving in. This will make a world of difference in allowing you to HONESTLY assess yourself and reduce local environment biases.</li>
             <li>Go to a place void of distractions where you can maintain this focus for at least 20min</li>
@@ -44,10 +35,6 @@ const Home = ({ countryCode, t }) => {
               I agree to the Terms of Service
             </label>
           </p>
-          <p>{t('home:paragraphs:select_language')}</p>
-          <Field name={t('common:language')}>
-            <Select name='languages' defaultValue={defaultLanguage} options={languageList} />
-          </Field>
           <div style={{ textAlign: 'center', paddingTop: '20px' }}>
             <Button value='Start Test' type='submit' background='rgb(255, 0, 128)' border='1px solid rgb(255, 0, 128)' />
           </div>
@@ -57,4 +44,4 @@ const Home = ({ countryCode, t }) => {
   )
 }
 
-export default withI18next(['home', 'common'])(Home)
+export default Home
